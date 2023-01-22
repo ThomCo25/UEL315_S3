22cd4904bcebdbd3a7d76bf9de8ebc64a3aafdec
Thomas Collin <thomas.collin@etu.unilim.fr>
2023-01-22 20:30:45 +0000

    txt

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -1,11 +1,5 @@
-import {
-    Prop,
-    Schema,
-    SchemaFactory
-  } from '@nestjs/mongoose';
-  import {
-    Document
-  } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
   
   export type UsersDocument = Users & Document;
   
